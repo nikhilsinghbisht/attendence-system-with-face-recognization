09752d7bb054d88aeba440bf578da123104b69a8
nikhil <nb9690@gmail.com>
2023-05-18 22:20:46 +0530

    first commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import face_recognition
 import os
 from datetime import datetime
+import time
 
 # from PIL import ImageGrab
 
@@ -78,6 +79,9 @@ while True:
             cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
             markAttendance(name)
+
+
+
 
     cv2.imshow('Webcam', img)
     cv2.waitKey(1)
